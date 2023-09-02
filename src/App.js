476993fa5e466e10dc06bc2e988a624231e28a89
jojo sexton <jojolsexton@gmail.com>
2023-09-02 13:48:58 +1200

    starting of my new pro porfolio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import  { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
+const App = () => {
+  return <>
+
+  
+  </>
 }
+
 
 export default App;
---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className = "profile-container">
+      <div className = 'profile-parent'>
+        <div className = "profile-details">
+          
+          {/* this is where we show the icon of git hub and linkedin  */}
+          <div classNAme = 'colz'>  
+            <a href='# '> <i className = 'fa facebook-square'></i> </a>
+          </div>
+          
+        </div>
+      </div>
+
+    </div>
+  )
+}
